96d31c8842cef0baf53e584c0a811f21cdda461f
Shreekant Bangar <109421128+shreekantbangar@users.noreply.github.com>
2023-03-25 06:58:04 +0530

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,19 @@
+const getcolor = () => {
+    // Hex Code
+    const randomNumber = Math.floor (Math.random() * 16777215);
+    const randomcode = "#" + randomNumber.toString(16);
+    console.log(randomNumber,randomcode);
+    document.body.style.backgroundColor = randomcode;
+    document.getElementById("color-code").innerText = randomcode;
+   
+    navigator.clipboard.writeText(randomcode);
+}
+
+// event call
+document.getElementById("btn").addEventListener(
+    "click",
+    getcolor
+)
+
+// initial call
+getcolor();
